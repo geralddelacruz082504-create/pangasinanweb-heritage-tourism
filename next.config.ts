ee9c873cb6acbe67fae2c2891ab390676e2f1771
Gerald <geralddelacruz082504@gmail.com>
2026-09-05 14:31:36 +0800

    Configure Next.js for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/pangasinanweb-heritage-tourism",
   images: {
     unoptimized: true,
   },
